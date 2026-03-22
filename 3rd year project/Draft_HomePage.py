64eefcfa904b1da9e4da4bc3e0df188e3ec7630c
Celine <B00160080@mytudublin.ie>
2026-03-22 17:57:21 +0000

    temp

---
+++ 3rd year project/Draft_HomePage.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    url = None
+    if request.method == "POST":
+        url = request.form.get("url")
+    
+    return render_template("index.html", url=url)
+
+if __name__ == "__main__":
+    app.run(debug=True)
